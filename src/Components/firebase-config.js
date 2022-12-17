9f2d500f52e4d4adad8c337823923d8f1b6e4732
jzf21 <jozefneelamkavil2003@gmail.com>
2022-12-17 18:37:22 +0530

    firebase added

---
+++ src/Components/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoutwBXiPr8u8xvqtU7Sr_TN1WerOWie8",
+  authDomain: "wreaders-29a5d.firebaseapp.com",
+  projectId: "wreaders-29a5d",
+  storageBucket: "wreaders-29a5d.appspot.com",
+  messagingSenderId: "916537269766",
+  appId: "1:916537269766:web:e9ce845dd37a29edd8ad87",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
